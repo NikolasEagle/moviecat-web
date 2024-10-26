a5648298df48a0195001ac4c40a5141d828003ad
NikolasEagle <eagle.dev.stack@gmail.com>
2024-10-26 18:11:37 +0400

    Сохранение компонентов

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,6 +4,11 @@ import styles from "./Search.module.scss";
 function Search() {
   const [searchPhrase, setSearchPhrase] = useState("");
 
+  let search = async () => {
+    try {
+    } catch (error) {}
+  };
+
   return (
     <div className={styles.search_panel}>
       <input
@@ -12,7 +17,7 @@ function Search() {
         value={searchPhrase}
         onChange={(event) => setSearchPhrase(event.target.value)}
       />
-      <button>Поиск</button>
+      <button onClick={search}>Поиск</button>
     </div>
   );
 }
